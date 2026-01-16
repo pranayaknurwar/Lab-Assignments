19babf4186b667ded0d8ef9cfe293c5a64a82a40
pranayaknurwar <pranayaknurwar.cse24@sbjit.edu.in>
2026-01-17 01:13:04 +0530

    done bhai aab mare se nhi hoga

--- practical_no_02/lab_assign_01.py
+++ practical_no_02/lab_assign_01.py
@@ -1,2 +1,17 @@
+# Simple Python program
 
-print("i will do you later")
+# Ask for user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Print greeting
+print(f"Hello, {name}!")
+
+# Check if user is minor or adult
+if age < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
+
+# End of program
+print("Have a great day!")
